096118189752a29540d17b21e04777c3662a6c16
Hank Richter <hankrichter97@gmail.com>
2023-06-19 15:38:43 -0400

    Cleanup comments and added StaleWhileRevalidate from workbox-strategies.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.error('putDb not implemented');
   const jateDb = await openDB("jate", 1);
@@ -23,7 +22,7 @@ export const putDb = async (content) => {
   console.log("result.value", result);
   return result;
 }
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   console.error('getDb not implemented');
   const jateDb = await openDB("jate", 1);
